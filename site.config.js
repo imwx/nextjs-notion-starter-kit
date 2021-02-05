0ae3e675a5751a358301f1b9f9ab189b4f788a14
Wx <asimove@126.com>
2021-02-05 17:36:07 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '0f13446060c74c738bdb05cd600d7a2e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '常用软件',
+  domain: 'soft.movenn.cn',
+  author: 'Movenn',
 
   // open graph metadata (optional)
   description: 'Example site description',
